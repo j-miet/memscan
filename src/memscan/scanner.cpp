6f3bfc7b21916caf7dbf509d346d54d0fe8a313c
j-miet <191247923+j-miet@users.noreply.github.com>
2025-08-09 17:03:48 +0300

    Add ui for int scanners
    Other:
    - added system("tasklist") call to display all IDs of running processes
    when user types "tasklist" during pID input prompt
    - improved readability of codebase
    - Scanner::intToString now returns long long to avoid crashes with large
    addresses

--- src/memscan/scanner.cpp
+++ src/memscan/scanner.cpp
@@ -39,7 +39,7 @@ std::vector<MemBlock> Scanner::createScan(int processId, int dataSize)
     return mbScan;
 };
 
-int Scanner::getMatchesCount(std::vector<MemBlock> mbScan) 
+int Scanner::getMatchesCount(std::vector<MemBlock>& mbScan) 
 {
     int count = 0;
 
@@ -51,7 +51,7 @@ int Scanner::getMatchesCount(std::vector<MemBlock> mbScan)
     return count;
 }
 
-int Scanner::stringToInt(std::string s)
+long long Scanner::stringToInt(std::string s)
 {
     int base = 10;
     bool negative = false;
@@ -60,6 +60,7 @@ int Scanner::stringToInt(std::string s)
     {
         base = 16;
         s.erase(0, 2);
+        return stoll(s, nullptr, base); // can be replaced with stoi/stol (int and long respectively)
     }
     else if (s.size() == 0)
     {
@@ -73,17 +74,17 @@ int Scanner::stringToInt(std::string s)
     
     for (auto ch : s)
     {   
-        if ((isdigit(ch) == false) ? true : false)
+        if ((!isdigit(ch)))
         { 
             return -1; // processId and dataSize are always non-negative so -1 can be used for discarding bad inputs
         }
     }
     if (negative)
     {
-        return -stoi(s, 0, base);
+        return -stoll(s, nullptr, base);
     }
 
-    return stoi(s, 0, base);
+    return stoll(s, nullptr, base);
 }
 
 StringScanner Scanner::createStringScanner(Condition startCondition)
@@ -99,8 +100,8 @@ StringScanner Scanner::createStringScanner(Condition startCondition)
 IntScanner Scanner::createIntScanner(Condition startCondition)
 {
     IntScanner intScan(m_scan);
-    //intScan.updateScan(startCondition, m_intVal);
-    //m_scan = intScan.memblocks();
+    intScan.updateScan(startCondition, m_intVal);
+    m_scan = intScan.memblocks();
 
     std::cout << "\r\n" << getMatchesCount(m_scan) << " matches found\n";
     return intScan;
@@ -115,11 +116,16 @@ void Scanner::uiNewScan()
 
     while(1)
     {
-        std::cout << "\r\nEnter the process id: ";
+        std::cout << "\r\nEnter the process id (or type [tasklist] to display all running tasks): ";
         std::getline(std::cin, input);
+        if (input == "tasklist")
+        {
+            system("tasklist");
+            continue;
+        }
         pId = stringToInt(input);
 
-        std::cout << "\r\nEnter the data size (1, 2, 4 or 8 bytes. Empty input means 8): ";
+        std::cout << "\r\nEnter the data size (1/2/4/8 for integers, s for strings. Empty input means 4): ";
         std::getline(std::cin, input);
         if (input[0] == 's')
         {
@@ -131,11 +137,11 @@ void Scanner::uiNewScan()
             dataSize = stringToInt(input);
             if (dataSize == -1)
             {   
-                dataSize = 8;
+                dataSize = 4;
             }
         }
 
-        std::cout << "\r\nEnter the start value, or empty input for unconditional search: ";
+        std::cout << "\r\nEnter the start value, or empty input to search all values: ";
         std::getline(std::cin, input);
         if (input.size() == 0)
         {
@@ -168,25 +174,26 @@ void Scanner::uiNewScan()
 
 // String UI
 
-void Scanner::uiPrintStringMatches(StringScanner strScan, int size) 
+void Scanner::uiPrintStringMatches(StringScanner& strScan, int size) 
 {
-    for (auto mb : strScan.memblocks())
+    uintptr_t address;
+
+    for (auto& mb : strScan.memblocks())
     {
         for (int offset = 0; offset < mb.size(); offset++) 
         {
             if (mb.isInSearch(offset)) 
             {
-                std::string sVal = strScan.readString(reinterpret_cast<uintptr_t>(mb.addr()) + offset, size);
-                std::cout << "0x";
-                std::cout << std::hex << reinterpret_cast<intptr_t>(mb.addr()) + offset << std::dec;
-                std::cout << "-> value: " << std::flush << sVal << std::flush;
-                std::cout << " | size: " << mb.size() << "\r" << std::endl;
+                address = reinterpret_cast<uintptr_t>(mb.addr()) + offset;
+                std::string sVal = strScan.readString(address, size);
+                std::cout << "0x" << std::hex << address << std::dec << " -> value: " << std::flush;
+                std::cout << sVal << std::flush << " | size: " << mb.size() << "\r" << std::endl;
             }
         }
     }
 }
 
-void Scanner::uiWriteString(StringScanner scanner)
+void Scanner::uiWriteString(StringScanner& scanner)
 {
     uintptr_t addr;
     std::string input;
@@ -197,12 +204,12 @@ void Scanner::uiWriteString(StringScanner scanner)
 
     std::cout << "\nEnter the value: ";
     std::cin >> input;
-    std::cout << "\r\n";
+    std::cout << "\r";
 
     scanner.writeString(addr, input);
 }
 
-int Scanner::openStringUi(StringScanner strScanner)
+int Scanner::openStringUi(StringScanner& strScanner)
 {
     std::string input;
     std::string sVal = m_strVal;
@@ -254,8 +261,80 @@ int Scanner::openStringUi(StringScanner strScanner)
 
 // Int UI
 
-/*
-int Scanner::openIntUi(IntScanner intScanner)
+void Scanner::uiWriteInt(IntScanner& scanner)
+{
+    uintptr_t addr;
+    std::string input;
+
+    std::cout << "Enter the address: ";
+    std::cin >> input;
+    addr = stringToInt(input);
+
+    std::cout << "\nEnter the value: ";
+    std::cin >> input;
+    std::cout << "\r";
+
+    int64_t val = stringToInt(input);
+    switch (scanner.memblocks()[0].dataSize())
+    {
+        case 1:
+            scanner.writeInt8(addr, static_cast<char>(val));
+            break;
+        case 2:
+            scanner.writeInt16(addr, static_cast<int16_t>(val));
+            break;
+        default:    
+        case 4:
+            scanner.writeInt32(addr, static_cast<int32_t>(val));
+            break;
+        case 8:
+            scanner.writeInt64(addr, val);
+            break;
+    }
+}
+
+void Scanner::uiPrintIntMatches(IntScanner& intScan) 
+{
+    uintptr_t address;
+    char valChar;
+    int16_t valInt16;
+    int32_t valInt32;
+    int64_t valInt64;
+
+    for (auto& mb : intScan.memblocks())
+    {
+        for (int offset = 0; offset < mb.size(); offset += mb.dataSize()) 
+        {
+            if (mb.isInSearch(offset)) 
+            {
+                address = reinterpret_cast<uintptr_t>(mb.addr()) + offset;
+                std::cout << "0x" << std::hex << address << std::dec << " -> value: " << std::flush;
+                switch (mb.dataSize())
+                {
+                    case 1:
+                        valChar = intScan.readInt8(address);
+                        std::cout << valChar;
+                        break;
+                    case 2:
+                        valInt16 = intScan.readInt16(address);
+                        std::cout << valInt16;
+                        break;
+                    case 4:
+                        valInt32 = intScan.readInt32(address);
+                        std::cout << valInt32;
+                        break;
+                    case 8:
+                        valInt64 = intScan.readInt64(address);
+                        std::cout << valInt64;
+                        break;
+                }
+                std::cout << std::flush << " | size: " << mb.size() << "\r" << std::endl;
+            }
+        }
+    }
+}
+
+int Scanner::openIntUi(IntScanner& intScanner)
 {
     std::string input;
     int64_t iVal = m_intVal;
@@ -272,12 +351,12 @@ int Scanner::openIntUi(IntScanner intScanner)
             "\r\n=>";
 
         std::cin >> input;
-        std::cin.ignore(1024, '\n'); // flush newline from buffer for std::getline
+        std::cin.ignore(1024, '\n');
         std::cout << "\r\n";
 
         switch (input[0])
         {
-            case 'i':            
+            case 'i':         
                 intScanner.updateScan(COND_INCREASED, iVal);
                 std::cout << getMatchesCount(intScanner.memblocks()) << " matches found\r\n";
                 break;
@@ -285,11 +364,11 @@ int Scanner::openIntUi(IntScanner intScanner)
                 intScanner.updateScan(COND_DECREASED, iVal);
                 std::cout << getMatchesCount(intScanner.memblocks()) << " matches found\r\n"; 
                 break;
-            case 'm':
-                uiPrintStringMatches(intScanner, iVal.size());
-                break;
             case 'p':
-                uiWriteString(intScanner);
+                uiPrintIntMatches(intScanner);
+                break;
+            case 'm':
+                uiWriteInt(intScanner);
                 break;
             case 'n':
                 return 1;
@@ -304,4 +383,3 @@ int Scanner::openIntUi(IntScanner intScanner)
         }
     }
 }
-*/
